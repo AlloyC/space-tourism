ae55a35ea9196b80aef8826788f12cf0cbd15fe8
john okeoghene <jonnyscience08@gmail.com>
2023-06-04 12:55:19 +0100

    modified index.html and created new files
    created js file and destination.html file

---
+++ scripts.js
@@ -0,0 +1,4 @@
+const HOME = document.getElementById("home");
+const DESTINATION = document.getElementById("destination");
+const CREW = document.getElementById("crew");
+const TECHNOLOGY = document.getElementById("technology");
